200f6790242aa53feb20e8ea2176a72c001e93d0
Dan Jakob Ofer <ofer987@gmail.com>
2016-02-24 09:40:50 -0500

    Created project TTC Notices
    TCC Notices tracks statuses of various bus, streetcar, and subway
    lines of the Toronto Transit Commission.

---
+++ ttc_notices/config/initializers/twitter.rb
@@ -0,0 +1,8 @@
+module Twitter
+  CLIENT = Twitter::Streaming::Client.new do |config|
+    config.consumer_key        = 'hV1JBfUJ9RVF8ylpeowXDvtQj'
+    config.consumer_secret     = 'v69EdxjYYVZ1YAgVdBzGp6ooFMkPxzWPyLqYnVT5xyzcatc1ya'
+    config.access_token        = '803725628-5tsKPyz3T9jDIBt5DpvsIWPkL9fJ5S91keikbjFn'
+    config.access_token_secret = 'uTlJ3XPU0nX3hu4NoyBtgWCDEjGj8KHSjIVEVaOcaS4hd'
+  end
+end
