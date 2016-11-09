5535aef144b12e2f6de5b77a651cad3b1d7014d4
Dan Jakob Ofer <ofer987@gmail.com>
2016-11-09 16:01:20 -0500

    Fixed the puma config file

--- ttc_notices/config/puma.rb
+++ ttc_notices/config/puma.rb
@@ -1,6 +1,4 @@
-# binding.pry
-# return if Rails.env.development?
-
+RAILS_ENV = ENV['RAILS_ENV'] || 'development'
 APP_DIR = File.expand_path("../..", __FILE__)
 SHARED_DIR = "#{APP_DIR}/shared"
 LOG_DIR = "#{SHARED_DIR}/log"
@@ -10,6 +8,9 @@ ERROR = "#{LOG_DIR}/puma.stderr.log"
 PIDS_DIR = "#{SHARED_DIR}/pids"
 PID = "#{PIDS_DIR}/puma.pid"
 STATE = "#{PIDS_DIR}/puma.state"
+
+SOCKETS_DIR = "#{SHARED_DIR}/sockets"
+SOCKET = "#{SOCKETS_DIR}/puma.sock"
 
 def setup
   workers Integer(ENV['WEB_CONCURRENCY'] || 1)
@@ -30,14 +31,14 @@ def setup
   activate_control_app
 
   preload_app!
-  environment Rails.env
+  environment RAILS_ENV
 
   on_worker_boot do
     require "active_record"
 
     ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
     ActiveRecord::Base.establish_connection(
-      YAML.load_file("#{APP_DIR}/config/database.yml")[Rails.env])
+      YAML.load_file("#{APP_DIR}/config/database.yml")[RAILS_ENV])
   end
 end
 
@@ -50,6 +51,9 @@ def create_files
   FileUtils.mkdir_p(PIDS_DIR) unless Dir.exists?(PIDS_DIR)
   FileUtils.touch(PID) unless File.exists?(PID)
   FileUtils.touch(STATE) unless File.exists?(STATE)
+
+  FileUtils.mkdir_p(SOCKETS_DIR) unless Dir.exists?(SOCKETS_DIR)
+  FileUtils.touch(SOCKET) unless File.exists?(SOCKET)
 end
 
 setup
