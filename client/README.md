5129d30f1dcca3c43fe30cbb9b9f17988a6d69c8
Dan Jakob Ofer <dan@ofer.to>
2017-11-29 20:49:13 -0500

    Replace the TypeScript client with the Elm client

--- client/README.md
+++
@@ -1,5 +0,0 @@
-# Installation
-
-1. `npm install`
-2. `typings install`
-2. `npm run serve`
--- client/src/app/Config.ts
+++
@@ -1,4 +0,0 @@
-export class Config {
-  static restbusUrl = 'http://localhost:3000';
-  static ttcNoticesUrl = 'http://localhost:3001'
-}
