9ab9d6dfcb0643979a5762c253e77c3def6ba181
Dan Jakob Ofer <dan@ofer.to>
2018-06-25 22:21:01 -0400

    Droplet Kit can spin up a new node

--- seedee/lib/seedee.rb
+++ seedee/lib/seedee.rb
@@ -1,5 +1,40 @@
-require "seedee/version"
+# frozen_string_literal: true
 
-module Seedee
-  # Your code goes here...
+require 'seedee/version'
+require 'droplet_kit'
+
+# Talk to Digital Ocean
+class Seedee
+  ACCESS_TOKEN = ENV['DIGITAL_OCEAN_TOKEN']
+
+  def new_droplet(name)
+    droplet = DropletKit::Droplet.new(
+      name: name.to_s.strip,
+      region: 'tor1',
+      image: 'ubuntu-16-04-x64',
+      size: 's-1vcpu-1gb',
+      ssh_keys: Array(ssh_keys)
+    )
+
+    await_active_droplet! client.droplets.create(droplet)
+  end
+
+  def await_active_droplet!(droplet)
+    client.droplets.find(id: droplet.id).tap do |result|
+      if result.status != 'active'
+        sleep(1)
+        return await_active_droplet!(droplet)
+      end
+    end
+  end
+
+  private
+
+  def client
+    @client ||= DropletKit::Client.new(access_token: ACCESS_TOKEN)
+  end
+
+  def ssh_keys
+    [15_237_447]
+  end
 end
