20009a91beec423a0d33012bf1a8138daa0cba58
Dan Jakob Ofer <dan@ofer.to>
2018-07-03 22:28:31 -0400

    Add chef-server to hosts file so can talk via SSL

--- seedee/lib/seedee.rb
+++ seedee/lib/seedee.rb
@@ -5,6 +5,8 @@ require 'active_support/all'
 
 # Top-level module
 module Seedee
+  ROOT = File.expand_path('../..', __FILE__)
+
   autoload :Farm, 'seedee/farm'
   autoload :DigitalOcean, 'seedee/digital_ocean'
   autoload :Chef, 'seedee/chef'
--- seedee/lib/seedee/digital_ocean.rb
+++ seedee/lib/seedee/digital_ocean.rb
@@ -7,15 +7,16 @@ module Seedee
   # Talk to Digital Ocean
   class DigitalOcean
     ACCESS_TOKEN = ENV['DIGITAL_OCEAN_TOKEN']
-
     TRAVIS_BUILD_DIR = ENV['TRAVIS_BUILD_DIR']
 
+    user_data = File.read(File.join(Seedee::ROOT, 'modify-hosts'))
     def new_droplet(name)
       droplet = DropletKit::Droplet.new(
         name: name.to_s.strip,
         region: 'tor1',
         image: 'ubuntu-16-04-x64',
         size: 's-1vcpu-1gb',
+        user_data: user_data,
         ssh_keys: Array(ssh_keys)
       )
 
