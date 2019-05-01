878242539ee398c97dd6fbc48911169f2e27f05c
Dan Jakob Ofer <dan@ofer.to>
2019-04-30 21:27:01 -0400

    Organise stations by line

--- restbus/app/models/ttc/train/lines.rb
+++ restbus/app/models/ttc/train/lines.rb
@@ -1,10 +1,547 @@
 module Ttc
   module Train
     LINES = {
-      'Yonge University Spadina' => 1,
-      'Bloor Danforth' => 2,
-      'Scarborough RT' => 3,
-      'Sheppard' => 4,
+      :'Yonge-University-Spadina' => {
+        id: 1,
+        name: 'Yonge-University-Spadina',
+        stations: {
+          :'Downsview Park' => {
+            id: 75,
+            name: 'Downsview Park',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Finch West' => {
+            id: 76,
+            name: 'Finch West',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'York University' => {
+            id: 77,
+            name: 'York University',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Pioneer Village' => {
+            id: 78,
+            name: 'Pioneer Village',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Highway 407' => {
+            id: 79,
+            name: 'Highway 407',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Vaughan Metropolitan Centre' => {
+            id: 80,
+            name: 'Vaughan Metropolitan Centre',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Sheppard West' => {
+            id: 1,
+            name: 'Sheppard West',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Wilson' => {
+            id: 2,
+            name: 'Wilson',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Yorkdale' => {
+            id: 3,
+            name: 'Yorkdale',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Lawrence West' => {
+            id: 4,
+            name: 'Lawrence West',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Glencairn' => {
+            id: 5,
+            name: 'Glencairn',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Eglinton West' => {
+            id: 6,
+            name: 'Eglinton West',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'St. Clair West' => {
+            id: 7,
+            name: 'St. Clair West',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Dupont' => {
+            id: 8,
+            name: 'Dupont',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Spadina' => {
+            id: 9,
+            name: 'Spadina',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'St. George' => {
+            id: 10,
+            name: 'St. George',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Museum' => {
+            id: 11,
+            name: 'Museum',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Queen\'s Park' => {
+            id: 12,
+            name: 'Queen\'s Park',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'St. Patrick' => {
+            id: 13,
+            name: 'St. Patrick',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Osgoode' => {
+            id: 14,
+            name: 'Osgoode',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'St. Andrew' => {
+            id: 15,
+            name: 'St. Andrew',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Union' => {
+            id: 16,
+            name: 'Union',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'King' => {
+            id: 17,
+            name: 'King',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Queen' => {
+            id: 18,
+            name: 'Queen',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Dundas' => {
+            id: 19,
+            name: 'Dundas',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'College' => {
+            id: 20,
+            name: 'College',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Wellesley' => {
+            id: 21,
+            name: 'Wellesley',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Bloor-Yonge' => {
+            id: 22,
+            name: 'Bloor-Yonge',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Rosedale' => {
+            id: 23,
+            name: 'Rosedale',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Summerhill' => {
+            id: 24,
+            name: 'Summerhill',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'St. Clair' => {
+            id: 25,
+            name: 'St. Clair',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Davisville' => {
+            id: 26,
+            name: 'Davisville',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Eglinton' => {
+            id: 27,
+            name: 'Eglinton',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Lawrence' => {
+            id: 28,
+            name: 'Lawrence',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'York mills' => {
+            id: 29,
+            name: 'York mills',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Sheppard-Yonge' => {
+            id: 30,
+            name: 'Sheppard-Yonge',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'North York Centre' => {
+            id: 31,
+            name: 'North York Centre',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Finch' => {
+            id: 32,
+            name: 'Finch',
+            latitude: 123,
+            longitude: 456
+          },
+        },
+      },
+
+      :'Bloor-Danforth' => {
+        id: 2,
+        name: 'Bloor-Danforth',
+        stations: {
+          :'Kipling' => {
+            id: 33,
+            name: 'Kipling',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Islington' => {
+            id: 34,
+            name: 'Islington',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Royal York' => {
+            id: 35,
+            name: 'Royal York',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Old Mill' => {
+            id: 36,
+            name: 'Old Mill',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Jane' => {
+            id: 37,
+            name: 'Jane',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Runnymede' => {
+            id: 38,
+            name: 'Runnymede',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'High Park' => {
+            id: 39,
+            name: 'High Park',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Keele' => {
+            id: 40,
+            name: 'Keele',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Dundas West' => {
+            id: 41,
+            name: 'Dundas West',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Lansdowne' => {
+            id: 42,
+            name: 'Lansdowne',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Dufferin' => {
+            id: 43,
+            name: 'Dufferin',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Ossington' => {
+            id: 44,
+            name: 'Ossington',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Christie' => {
+            id: 45,
+            name: 'Christie',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Bathurst' => {
+            id: 46,
+            name: 'Bathurst',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Spadina' => {
+            id: 47,
+            name: 'Spadina',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'St. George' => {
+            id: 48,
+            name: 'St. George',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Bay' => {
+            id: 49,
+            name: 'Bay',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Bloor-Yonge' => {
+            id: 50,
+            name: 'Bloor-Yonge',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Sherbourne' => {
+            id: 51,
+            name: 'Sherbourne',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Castle Frank' => {
+            id: 52,
+            name: 'Castle Frank',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Broadview' => {
+            id: 53,
+            name: 'Broadview',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Chester' => {
+            id: 54,
+            name: 'Chester',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Pape' => {
+            id: 55,
+            name: 'Pape',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Donlands' => {
+            id: 56,
+            name: 'Donlands',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Greenwood' => {
+            id: 57,
+            name: 'Greenwood',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Coxwell' => {
+            id: 58,
+            name: 'Coxwell',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Woodbine' => {
+            id: 59,
+            name: 'Woodbine',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Main Street' => {
+            id: 60,
+            name: 'Main Street',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Victoria Park' => {
+            id: 61,
+            name: 'Victoria Park',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Warden' => {
+            id: 62,
+            name: 'Warden',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Kennedy' => {
+            id: 63,
+            name: 'Kennedy',
+            latitude: 123,
+            longitude: 456
+          }
+        }
+      },
+
+      :'Scarborough-RT' => {
+        id: 3,
+        name: 'Scarborough RT',
+        stations: {}
+      },
+
+      :'Sheppard' => {
+        id: 4,
+        name: 'Sheppard',
+        stations: {
+          :'Sheppard-Yonge' => {
+            id: 64,
+            name: 'Sheppard-Yonge',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Bayview' => {
+            id: 65,
+            name: 'Bayview',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Bessarion' => {
+            id: 66,
+            name: 'Bessarion',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Leslie' => {
+            id: 67,
+            name: 'Leslie',
+            latitude: 123,
+            longitude: 456
+          },
+
+          :'Don Mills' => {
+            id: 68,
+            name: 'Don Mills',
+            latitude: 123,
+            longitude: 456
+          }
+        }
+      }
     }.freeze
   end
 end
