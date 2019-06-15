e0800799bf1eee05b26722c93eb8c60aec1245cc
Dan Jakob Ofer <dan@ofer.to>
2019-06-15 14:56:11 -0400

    Add iOS project

---
+++ ios/TransitTips/TransitTips/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TransitTips
+//
+//  Created by Dan Jakob Ofer on 2019-06-15.
+//  Copyright © 2019 Dan Jakob Ofer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
