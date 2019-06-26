79cb66f3d9a139237f32bc0611b4191d7e3e0e90
Dan Jakob Ofer <dan@ofer.to>
2019-06-26 08:56:21 -0400

    Decode JSON to models

--- ios/TransitTips/TransitTips/Schedule.swift
+++ ios/TransitTips/TransitTips/Schedule.swift
@@ -8,35 +8,7 @@
 
 import Foundation
 
-class Schedule : Decodable {
-    struct Key : CodingKey {
-        var stringValue: String
-        init(stringValue: String) { self.stringValue = stringValue }
-        
-        var intValue: Int?
-        init?(intValue: Int) { return nil }
-        
-        static let lines = Key(stringValue: "lines")
-        static let latitude = Key(stringValue: "latitude")
-        static let longitude = Key(stringValue: "longitude")
-    }
-    
-    var routes: [Route]
-    var requestedLocation: Location
-    
-    init() {
-        self.routes = Array()
-        self.requestedLocation = Location(0.00, 1.012)
-    }
-    
-    required public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: Key.self)
-        self.requestedLocation = Location(
-            try container.decode(Double.self, forKey: .latitude),
-            try container.decode(Double.self, forKey: .longitude)
-        )
-        self.routes = try container.decode([Route].self, forKey: .lines)
-//        self.routes = try container.nestedContainer(keyedBy: [Route.Key].self, forKey: Schedule.Key.lines)
-//        routes = Array()
-    }
+class ScheduleModel {
+    var routes: [RouteModel] = [RouteModel]()
+    var requestedLocation: LocationModel = LocationModel(0.00, 0.00)
 }
